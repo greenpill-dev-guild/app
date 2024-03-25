f907940136519abdb7af497266c8b03264e16ab1
Afo <obaone01@gmail.com>
2024-03-24 22:16:08 -0700

    updated next intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
         NEXT_WEB3_STORAGE_TOKEN: process.env.NEXT_WEB3_STORAGE_TOKEN
     }
 };
-module.exports = nextConfig;
+
+export default withNextIntl(nextConfig);
