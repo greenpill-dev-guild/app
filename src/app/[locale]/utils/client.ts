0840e35cb71c8beebe3c5a03dc61a8f1587a955f
Afo <obaone01@gmail.com>
2024-03-24 22:32:44 -0700

    removed blockchain aspects

--- src/app/[locale]/utils/client.ts
+++
@@ -1,8 +0,0 @@
-import { createPublicClient, http } from "viem";
-import { getChain } from "../config/network.config";
-
-const transport = http(process.env.NEXT_PUBLIC_CHAIN_RPC);
-export const ViemClient = createPublicClient({
-  chain: getChain(),
-  transport,
-});
