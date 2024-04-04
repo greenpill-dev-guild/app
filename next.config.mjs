549fd4667ef9506be64dda58c1c3e447e5ba0c23
Afo <obaone01@gmail.com>
2024-04-03 21:57:48 -0700

    added pwa plugin to next

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,29 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+import withPWAInit from "@ducanh2912/next-pwa";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_WEB3_STORAGE_TOKEN: process.env.NEXT_WEB3_STORAGE_TOKEN
-    }
+  env: {
+    NEXT_WEB3_STORAGE_TOKEN: process.env.NEXT_WEB3_STORAGE_TOKEN,
+  },
 };
 
-export default withNextIntl(nextConfig);
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = withPWAInit({
+      dest: "public",
+    });
+
+    return withNextIntl(withPWA(nextConfig));
+  }
+
+  return withNextIntl(nextConfig);
+};
+
+export default nextConfigFunction;
