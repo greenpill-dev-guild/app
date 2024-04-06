76e393559fcab9f816c581ad741257741a51bfdc
Afo <obaone01@gmail.com>
2024-04-06 11:20:48 -0700

    added react tailwind select to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "node_modules/preline/dist/*.js",
+    "node_modules/react-tailwindcss-select/dist/index.esm.js",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('preline/plugin'),
-  ]
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
