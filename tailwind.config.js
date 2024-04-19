90be6ed5cec53bef46c539af4564c6dd781fdd6b
Afo <obaone01@gmail.com>
2024-04-19 01:30:33 -0700

    added date component added toast component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/preline/dist/*.js",
-    "node_modules/react-tailwindcss-select/dist/index.esm.js",
+    "./node_modules/react-tailwindcss-select/dist/index.esm.js",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: ["light"],
   },
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
